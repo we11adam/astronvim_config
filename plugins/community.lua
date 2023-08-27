5203c579ef8e8ecb1a7d0e052c3adfdeffe4d706
Adam L <skyros@gmail.com>
2023-08-27 20:20:04 +0800

    feat: community pack for bash/go/html-css/python/toml/yaml/typescript

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,6 +1,13 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.yaml" },
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
